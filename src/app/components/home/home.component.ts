09c9def859b0d0650a8d149e9eb2231739e0df0e
Jônatas Venâncio <jonatasvoliveira2015@gmail.com>
2026-03-28 14:25:06 -0300

    Remove app-contact

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,12 +5,11 @@ import { VisionComponent } from "../vision/vision.component";
 import { ServicesComponent } from "../services/services.component";
 import { DifferentialComponent } from '../differential/differential.component';
 import { PartnersComponent } from '../partners/partners.component';
-import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, VisionComponent, ServicesComponent, DifferentialComponent, PartnersComponent, ContactComponent],
+  imports: [HeaderComponent, NgOptimizedImage, VisionComponent, ServicesComponent, DifferentialComponent, PartnersComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
